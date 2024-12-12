2bbac50d5ef3705be225761ef64ac09b32b19408
Killiivalavan <kavalavan04@gmail.com>
2024-12-12 19:29:03 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   };
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App bg-white dark:bg-black min-h-screen transition-colors duration-300">
         <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
         <Routes>
